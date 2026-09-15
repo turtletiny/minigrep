2a20c9559e235dc7c82fae37274384064b063064
Daniel <daniel.lukins1@gmail.com>
2026-09-15 14:02:39 +1000

    use stderr for error msgs

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,55 @@
+use minigrep::{search, search_case_insensitive};
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(&args);
 
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(69);
+    });
 
-    println!("searching for {}", config.query);
-    println!("in file {}", config.file_path);
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(69);
+    }
+}
 
-    let contents =
-        fs::read_to_string(&config.file_path).expect("Should have been able to read the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
 }
 
 struct Config {
-    query: String,
-    file_path: String,
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    Config {
-        query: args[1].clone(),
-        file_path: args[2].clone(),
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        Ok(Self {
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+            ignore_case: env::var("IGNORE_CASE").is_ok(),
+        })
     }
 }
